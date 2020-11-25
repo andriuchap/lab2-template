344b4f4fbed93675a58d177e4bfd45e93036f6de
Andrius <andr.paulauskas@gmail.com>
2020-11-25 15:15:25 +0200

    initial commit

---
+++ app/src/main/java/edu/ktu/lab2template/Adapter.kt
@@ -0,0 +1,26 @@
+package edu.ktu.lab2template
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v)
+    {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
